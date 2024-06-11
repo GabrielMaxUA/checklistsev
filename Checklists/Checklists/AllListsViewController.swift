0bc8074a77bc9296e78bd7f7f98c7ce64f7e3a39
Max Gabriel <Max.Gabriel@triosstudent.com>
2024-06-10 20:31:28 -0400

    added List of lists Ch16.

---
+++ Checklists/Checklists/AllListsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AllListsViewController.swift
+//  Checklists
+//
+//  Created by Max Gabriel on 2024-06-10.
+//
+
+import UIKit
+
+class AllListsViewController: UITableViewController {
+    let cellIdentifier = "ChecklistCell"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 3
+    }
+
+    override func tableView(
+      _ tableView: UITableView,
+      cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(
+        withIdentifier: cellIdentifier, for: indexPath)
+      cell.textLabel!.text = "List \(indexPath.row)"
+      return cell
+    }
+
+}
